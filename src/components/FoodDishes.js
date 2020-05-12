f85dbc826704fda886587386a8cddbb66ac46eab
Alfonsina <alfonsina.l@gmail.com>
2020-05-11 20:06:14 -0400

    First commit, food app made consuming the edamam API

---
+++ src/components/FoodDishes.js
@@ -0,0 +1,38 @@
+import React, {Fragment} from 'react';
+import Grid from '@material-ui/core/Grid';
+import FoodDish from './FoodDish';
+import NoResults from './NoResults';
+
+const FoodDishes = ({hits}) => (
+  <Fragment>
+    {
+      hits.length ? 
+        <div className="root">
+          <Grid container spacing={3} justify="center">
+            {
+              hits.map(hit => {
+                const {uri, label, image, calories, ingredients} = hit.recipe;
+                return (
+                  <FoodDish
+                    key={uri}
+                    label={label}
+                    image={image}
+                    calories={image}
+                    ingredients={ingredients}
+                  />
+                )
+              })
+            }
+          </Grid>
+        </div>
+      :
+        <NoResults />
+    }
+  </Fragment>
+);
+
+FoodDishes.displayName = 'FoodDishes';
+
+export default FoodDishes;
+
+
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <p>&copy; {new Date().getFullYear()} - Alfonsina Lizardo</p>
+  </footer>
+)
+
+Footer.displayName = "Footer";
+
+export default Footer;
